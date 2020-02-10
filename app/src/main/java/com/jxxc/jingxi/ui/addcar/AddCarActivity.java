5a4bf4a776d160da50b1aa4363b5fdf1f7d6c55a
lienyin <lienyin@zhizukj.com>
2020-02-10 16:59:35 +0800

    添加

--- app/src/main/java/com/jxxc/jingxi/ui/addcar/AddCarActivity.java
+++ app/src/main/java/com/jxxc/jingxi/ui/addcar/AddCarActivity.java
@@ -1,12 +1,21 @@
 package com.jxxc.jingxi.ui.addcar;
 
 
+import android.inputmethodservice.KeyboardView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.jxxc.jingxi.R;
 import com.jxxc.jingxi.mvp.MVPBaseActivity;
 import com.jxxc.jingxi.utils.AnimUtils;
+import com.jxxc.jingxi.utils.KeyboardUtil;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -23,6 +32,9 @@ public class AddCarActivity extends MVPBaseActivity<AddCarContract.View, AddCarP
     TextView tv_back;
     @BindView(R.id.tv_title)
     TextView tv_title;
+    @BindView(R.id.edit_text)
+    EditText mEditText;
+    private KeyboardUtil keyboardUtil;
     @Override
     protected int layoutId() {
         return R.layout.add_car_activity;
@@ -31,6 +43,36 @@ public class AddCarActivity extends MVPBaseActivity<AddCarContract.View, AddCarP
     @Override
     public void initData() {
         tv_title.setText("添加车辆");
+        mEditText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                if (keyboardUtil == null) {
+                    keyboardUtil = new KeyboardUtil(AddCarActivity.this, mEditText);
+                    keyboardUtil.hideSoftInputMethod();
+                    keyboardUtil.showKeyboard();
+                } else {
+                    keyboardUtil.showKeyboard();
+                }
+                return false;
+            }
+        });
+        mEditText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.i("字符变换后", "afterTextChanged");
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.i("字符变换前", s + "-" + start + "-" + count + "-" + after);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.i("字符变换中", s + "-" + "-" + start + "-" + before + "-" + count);
+            }
+        });
     }
 
     @OnClick({R.id.tv_back})
@@ -42,5 +84,17 @@ public class AddCarActivity extends MVPBaseActivity<AddCarContract.View, AddCarP
                 break;
             default:
         }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (keyboardUtil.isShow()) {
+                keyboardUtil.hideKeyboard();
+            } else {
+                finish();
+            }
+        }
+        return false;
     }
 }
