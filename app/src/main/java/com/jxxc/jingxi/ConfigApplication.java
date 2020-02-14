43905ff7baaf5f19f8db06122988ad47895e98c6
lienyin <lienyin@zhizukj.com>
2020-02-14 22:21:53 +0800

    添加

--- app/src/main/java/com/jxxc/jingxi/ConfigApplication.java
+++ app/src/main/java/com/jxxc/jingxi/ConfigApplication.java
@@ -11,6 +11,9 @@ import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
 import com.bumptech.glide.Glide;
 import com.jxxc.jingxi.http.Constants;
 import com.jxxc.jingxi.http.DensityHelper;
@@ -59,6 +62,13 @@ public class ConfigApplication extends MultiDexApplication implements Serializab
         instans = this;
         mContext = instans.getApplicationContext();
         MultiDex.install(this);
+        //-------------初始化百度地图-------------------------
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        SDKInitializer.initialize(this);
+        //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
+        //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
+        SDKInitializer.setCoordType(CoordType.BD09LL);
+        //--------------------------------------------------
         ISNav.getInstance().init(new ImageLoader() {
             @Override
             public void displayImage(Context context, String path, ImageView imageView) {
--- app/src/main/java/com/jxxc/jingxi/ui/mapjingsi/MapJingSiActivity.java
+++ app/src/main/java/com/jxxc/jingxi/ui/mapjingsi/MapJingSiActivity.java
@@ -4,10 +4,23 @@ package com.jxxc.jingxi.ui.mapjingsi;
 import android.view.View;
 import android.widget.TextView;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 import com.jxxc.jingxi.R;
 import com.jxxc.jingxi.mvp.MVPBaseActivity;
 import com.jxxc.jingxi.utils.AnimUtils;
+import com.jxxc.jingxi.utils.SPUtils;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -24,8 +37,14 @@ public class MapJingSiActivity extends MVPBaseActivity<MapJingSiContract.View, M
     TextView tv_back;
     @BindView(R.id.tv_title)
     TextView tv_title;
-//    @BindView(R.id.mv_jingsi)
-//    MapView mv_jingsi;
+    @BindView(R.id.mv_jingsi)
+    MapView mMapView;
+    private BaiduMap mBaiduMap;
+    private LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+    private boolean isFirstLoc = true; // 是否首次定位
+    private double locationLatitude = 0;//当前经度
+    private double locationLongitude = 0;//当前纬度
     @Override
     protected int layoutId() {
         return R.layout.map_jing_si_activity;
@@ -34,6 +53,95 @@ public class MapJingSiActivity extends MVPBaseActivity<MapJingSiContract.View, M
     @Override
     public void initData() {
         tv_title.setText("附近技师");
+        initMap();
+    }
+
+    private void initMap(){
+        //获取地图控件引用
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);//普通地图
+        mBaiduMap.setMyLocationEnabled(true);// 开启定位图层
+        mMapView.showZoomControls(false);// 不显示地图缩放控件（+-按钮）
+        //自定义地图定位图片
+        BitmapDescriptor mCurrentMarker = BitmapDescriptorFactory.fromResource(R.mipmap.icon_79);
+        MyLocationConfiguration mLocationConfiguration = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL,
+                false, mCurrentMarker, 0, 0);
+        mBaiduMap.setMyLocationConfiguration(mLocationConfiguration);
+        //默认显示普通地图
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        //配置定位SDK参数
+        initLocation();
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+        //开启定位
+        mLocationClient.start();
+        //图片点击事件，回到定位点
+        mLocationClient.requestLocation();
+    };
+    //配置定位SDK参数
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span = 1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation
+        // .getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);
+        option.setOpenGps(true); // 打开gps
+        //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
+
+    //实现BDLocationListener接口,BDLocationListener为结果监听接口，异步获取定位结果
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // 构造定位数据
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(100).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            // 设置定位数据
+            mBaiduMap.setMyLocationData(locData);
+            // 当不需要定位图层时关闭定位图层
+            //mBaiduMap.setMyLocationEnabled(false);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                locationLatitude = location.getLatitude();
+                locationLongitude = location.getLongitude();
+
+                if ("4.9E-324".equals(locationLongitude) && "4.9E-324".equals(locationLatitude)) {
+                    toast(MapJingSiActivity.this, "百度地图定位失败");
+                } else if ("5e-324".equals(locationLongitude) && "5e-324".equals(locationLatitude)) {
+                    toast(MapJingSiActivity.this, "百度地图定位失败");
+                } else {
+                    String lat = SPUtils.get(MapJingSiActivity.this, "lat", "");
+                    String lng = SPUtils.get(MapJingSiActivity.this, "lng", "");
+                    if (!"".equals(lat) && !"".equals(lng)) {
+                        SPUtils.remove(MapJingSiActivity.this, lat);
+                        SPUtils.remove(MapJingSiActivity.this, lng);
+                    }
+                    //保存经纬度
+                    SPUtils.put("lat", location.getLatitude());
+                    SPUtils.put("lng", location.getLongitude());
+                }
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(16.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+        }
     }
 
     @OnClick({R.id.tv_back})
@@ -45,5 +153,25 @@ public class MapJingSiActivity extends MVPBaseActivity<MapJingSiContract.View, M
                 break;
             default:
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+        mLocationClient.stop();
     }
 }
