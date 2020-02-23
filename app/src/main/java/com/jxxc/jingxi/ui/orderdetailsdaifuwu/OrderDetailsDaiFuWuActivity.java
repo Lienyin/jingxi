c54cf983de5c4e2711eeb47cc1c77af5dc8ca772
lienyin <lienyin@zhizukj.com>
2020-02-23 16:57:05 +0800

    添加

--- app/src/main/java/com/jxxc/jingxi/ui/orderdetailsdaifuwu/OrderDetailsDaiFuWuActivity.java
+++ app/src/main/java/com/jxxc/jingxi/ui/orderdetailsdaifuwu/OrderDetailsDaiFuWuActivity.java
@@ -6,6 +6,26 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.geocode.GeoCoder;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeOption;
+import com.baidu.mapapi.search.route.BikingRouteResult;
+import com.baidu.mapapi.search.route.DrivingRouteResult;
+import com.baidu.mapapi.search.route.IndoorRouteResult;
+import com.baidu.mapapi.search.route.MassTransitRouteResult;
+import com.baidu.mapapi.search.route.OnGetRoutePlanResultListener;
+import com.baidu.mapapi.search.route.RoutePlanSearch;
+import com.baidu.mapapi.search.route.TransitRouteResult;
+import com.baidu.mapapi.search.route.WalkingRouteResult;
 import com.hss01248.dialog.StyledDialog;
 import com.jxxc.jingxi.R;
 import com.jxxc.jingxi.adapter.OrderDetailsDataAdapter;
@@ -13,9 +33,11 @@ import com.jxxc.jingxi.entity.backparameter.MyOrderEntity;
 import com.jxxc.jingxi.entity.backparameter.OrderEntity;
 import com.jxxc.jingxi.http.ZzRouter;
 import com.jxxc.jingxi.mvp.MVPBaseActivity;
+import com.jxxc.jingxi.ui.mapjingsi.MapJingSiActivity;
 import com.jxxc.jingxi.utils.AnimUtils;
 import com.jxxc.jingxi.utils.AppUtils;
 import com.jxxc.jingxi.utils.GlideImgManager;
+import com.jxxc.jingxi.utils.SPUtils;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -58,9 +80,18 @@ public class OrderDetailsDaiFuWuActivity extends MVPBaseActivity<OrderDetailsDai
     ImageView iv_jishi_hand;
     @BindView(R.id.gv_fuwu_data)
     GridView gv_fuwu_data;
+    @BindView(R.id.mv_map)
+    MapView mMapView;
+    private BaiduMap mBaiduMap;
+    private LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+    private boolean isFirstLoc = true; // 是否首次定位
+    private double locationLatitude = 0;//当前经度
+    private double locationLongitude = 0;//当前纬度
     private OrderEntity orderEntity = new OrderEntity();
-    private MyOrderEntity myOrderEntity;
+    private String orderId;
     private OrderDetailsDataAdapter adapter;
+    private RoutePlanSearch mSearch;
     @Override
     protected int layoutId() {
         return R.layout.order_details_dai_fu_wu_activity;
@@ -69,9 +100,10 @@ public class OrderDetailsDaiFuWuActivity extends MVPBaseActivity<OrderDetailsDai
     @Override
     public void initData() {
         tv_title.setText("订单详情");
-        myOrderEntity = ZzRouter.getIntentData(this,MyOrderEntity.class);
+        orderId = ZzRouter.getIntentData(this,String.class);
         StyledDialog.buildLoading("数据加载中").setActivity(this).show();
-        mPresenter.getOrder(myOrderEntity.orderId);
+        mPresenter.getOrder(orderId);
+        initMap();
     }
 
     @OnClick({R.id.tv_back,R.id.tv_details_call_phone,R.id.tv_details_cancel_order,R.id.tv_details_cui_order})
@@ -114,5 +146,117 @@ public class OrderDetailsDaiFuWuActivity extends MVPBaseActivity<OrderDetailsDai
         tv_details_order_memo.setText(data.remark);
         tv_details_order_coupon.setText("-￥"+data.discountsPrice);
         tv_details_order_money.setText("￥"+data.price);
+    }
+
+    private void initMap(){
+        //获取地图控件引用
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);//普通地图
+        mBaiduMap.setMyLocationEnabled(true);// 开启定位图层
+        mMapView.showZoomControls(false);// 不显示地图缩放控件（+-按钮）
+        //自定义地图定位图片
+//        BitmapDescriptor mCurrentMarker = BitmapDescriptorFactory.fromResource(R.mipmap.icon_79);
+//        MyLocationConfiguration mLocationConfiguration = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL,
+//                false, mCurrentMarker, 0, 0);
+        // mBaiduMap.setMyLocationConfiguration(mLocationConfiguration);
+        //默认显示普通地图
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        //配置定位SDK参数
+        initLocation();
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+        //开启定位
+        mLocationClient.start();
+        //图片点击事件，回到定位点
+        mLocationClient.requestLocation();
+    };
+    //配置定位SDK参数
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span = 1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation
+        // .getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);
+        option.setOpenGps(true); // 打开gps
+        //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
+
+    //实现BDLocationListener接口,BDLocationListener为结果监听接口，异步获取定位结果
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // 构造定位数据
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(100).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            // 设置定位数据
+            mBaiduMap.setMyLocationData(locData);
+            // 当不需要定位图层时关闭定位图层
+            //mBaiduMap.setMyLocationEnabled(false);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                locationLatitude = location.getLatitude();//当前经纬度
+                locationLongitude = location.getLongitude();
+
+                if ("4.9E-324".equals(locationLongitude) && "4.9E-324".equals(locationLatitude)) {
+                    toast(OrderDetailsDaiFuWuActivity.this, "百度地图定位失败");
+                } else if ("5e-324".equals(locationLongitude) && "5e-324".equals(locationLatitude)) {
+                    toast(OrderDetailsDaiFuWuActivity.this, "百度地图定位失败");
+                } else {
+                    String lat = SPUtils.get(OrderDetailsDaiFuWuActivity.this, "lat", "");
+                    String lng = SPUtils.get(OrderDetailsDaiFuWuActivity.this, "lng", "");
+                    if (!"".equals(lat) && !"".equals(lng)) {
+                        SPUtils.remove(OrderDetailsDaiFuWuActivity.this, lat);
+                        SPUtils.remove(OrderDetailsDaiFuWuActivity.this, lng);
+                    }
+                    //保存经纬度
+                    SPUtils.put("lat", location.getLatitude());
+                    SPUtils.put("lng", location.getLongitude());
+                }
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(16.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+
+//                mSearch = RoutePlanSearch.newInstance();
+//                mSearch.setOnGetRoutePlanResultListener(listener);
+            }
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+        mLocationClient.stop();
     }
 }
