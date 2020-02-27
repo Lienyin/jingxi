a8abbb5485cf538ce1ffa8c9d7c5319b92c4af7d
lienyin <lienyin@zhizukj.com>
2020-02-27 22:40:06 +0800

    添加

--- app/src/main/java/com/jxxc/jingxi/ui/discountcoupon/coupontype/adapter/MyCouponAdapter.java
+++ app/src/main/java/com/jxxc/jingxi/ui/discountcoupon/coupontype/adapter/MyCouponAdapter.java
@@ -10,6 +10,7 @@ import com.jxxc.jingxi.R;
 import com.jxxc.jingxi.entity.backparameter.MyCoupon;
 import com.jxxc.jingxi.utils.AppUtils;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 /**
@@ -24,14 +25,23 @@ public class MyCouponAdapter extends BaseQuickAdapter<MyCoupon, BaseViewHolder> 
 
     @Override
     protected void convert(BaseViewHolder helper, MyCoupon item) {
+        //优惠券类型 0无门槛减N 1满N减N 2折扣券
+        if (item.couponRuleType==0){
+            helper.setText(R.id.tv_money_coupon, "￥"+new DecimalFormat("0.00").format(item.money));
+        }else if (item.couponRuleType == 1){
+            helper.setText(R.id.tv_money_coupon, "￥"+new DecimalFormat("0.00").format(item.money));
+        }else{
+            helper.setText(R.id.tv_money_coupon, new DecimalFormat("0.00").format(item.discount)+"折");
+        }
+
         helper.setText(R.id.tv_name_coupon, (AppUtils.isEmpty(item.counponName)?"未知":item.counponName));
         helper.setText(R.id.tv_date_coupon, "有效期至："+item.startTime+"~"+item.endTime);
-        helper.setText(R.id.tv_money_coupon, "￥"+item.money);
+
 
         if (item.status == 1 || item.status == 2){
-            helper.setTextColor(R.id.tv_money_coupon,mContext.getResources().getColor(R.color.help_button_view));
+            helper.setBackgroundColor(R.id.ll_coupon_bg,mContext.getResources().getColor(R.color.help_button_view));
         }else{
-            helper.setTextColor(R.id.tv_money_coupon,mContext.getResources().getColor(R.color.white));
+            helper.setBackgroundColor(R.id.ll_coupon_bg,mContext.getResources().getColor(R.color.coupon_bg));
         }
     }
 }
